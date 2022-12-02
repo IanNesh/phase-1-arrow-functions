b74b69706f9e994eaadeb78d42249bc9b0eb082b
Ian <muneneian5@gmail.com>
2022-12-02 10:40:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function (){
+    const num1 = 2000
+    const num2 = 100
+    return num1 / num2
+}
+divide()
+const square = (num1) => {
+    return num1*num1
+}
+square()
+const add = () => {
+    let a = 3
+    let b = 4
+    return a + b
+}
